1af2fa90dd025da96abe188610704160bf9d51b5
hsusmita <hsusmita4@gmail.com>
2013-05-07 11:09:24 +0530

    Project files added

---
+++ FoldingViewController/FoldingViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FoldingViewController
+//
+//  Created by devmac46  on 07/05/13.
+//  Copyright (c) 2013 hsusmita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FoldingViewController/FoldingViewController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FoldingViewController
+//
+//  Created by devmac46  on 07/05/13.
+//  Copyright (c) 2013 hsusmita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
